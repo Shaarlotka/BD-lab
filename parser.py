4a66281ced33708c3972bfb45c7f6b4d127583dc
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2021-06-12 19:22:41 +0300

    parser ссылок на оф изображения карточек
    скачивайте сами, они в глупом формате, который не парсится >:(

---
+++ parser.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
+
+url = 'https://gatherer.wizards.com/Pages/Search/Default.aspx?name=+['
+
+def getCardImage(url, cardName):
+    sleep(2)
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
+
+    url += cardName + ']'
+
+    responce = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(responce.content, 'html.parser')
+    items = soup.findAll('div', class_='cardList')
+    comps = []
+
+    for i, item in enumerate(items):
+        comps.append(item.find('img').get('src'))
+        break
+
+    if comps:
+        comps[0] = 'https://gatherer.wizards.com' + comps[0][5:]
+
+    return comps #при получении значения тоже проверяй, не пустой ли массив
+                 #если пустой - отправляй строку "NULL", чтобы функция в sql поставила все по дефолту
+                 #если не пустой то все норм
+
+getCardImage(url, 'bolas') #это просто пример - 'bolas' типа имя карты, которое ввел пользователь при добавлении данных
